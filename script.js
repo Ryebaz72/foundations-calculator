12fb5ebc8a8ddb452234c79deb083d7cbace31d5
Kenneth Rough <kennethrough@gmail.com>
2024-10-16 10:37:32 +0100

    Add operate function to call individual calculate functions

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(num1, num2, operator) {
+    let result = 0;
+    switch (operator) {
+        case "+":
+            result = add(num1, num2);
+            break;
+        case "-":
+            result = subtract(num1, num2);
+            break;
+        case "*":
+            result = multiply(num1, num2);
+            break;
+        case "/":
+            result = divide(num1, num2);
+            break;
+        default:
+            break;
+    }
+    return result;
+}
